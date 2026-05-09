978753285798b1ad00e4c5803299f574a111cb5d
liyu <1179783112@qq.com>
2026-05-09 19:27:50 +0800

    chore(deps): 更新依赖项并添加新的 Element Plus 组件类型定义
    - 更新 Vite 依赖元数据中的浏览器哈希值
    - 更新 Pinia、Vue、Vue Router 和 Element Plus 相关包的文件哈希
    - 添加 ElAlert 和 ElEmpty 组件到全局组件类型定义中
    - 移除旧的代码块并添加新的代码块到依赖元数据
    - 更新 Element Plus 样式相关组件的哈希值
    - 为新添加的组件添加相应的 CSS 样式导入

--- frontend/src/components.d.ts
+++ frontend/src/components.d.ts
@@ -11,11 +11,13 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAlert: typeof import('element-plus/es')['ElAlert']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCheckboxGroup: typeof import('element-plus/es')['ElCheckboxGroup']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
+    ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
